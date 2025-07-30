b6b81c9e3b77f54d796c37a1333ca17747d762a4
Vali Malinoiu <x4139@Valis-MacBook-Pro.local>
2025-07-30 14:21:53 +0300

    create tweet reply

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/imperatrona/twitter-scraper
+module github.com/0x4139/twitter-scraper
 
 go 1.16
 
--- tweet.go
+++ tweet.go
@@ -45,6 +45,88 @@ func (newTweet *newTweet) parse() *Tweet {
 	}
 	return tw
 }
+func (s *Scraper) CreateReply(tweet NewTweet, tweetID string) (*Tweet, error) {
+	req, err := s.newRequest("POST", "https://twitter.com/i/api/graphql/oB-5XsHNAbjvARJEc8CZFw/CreateTweet")
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("content-type", "application/json")
+
+	media_entities := []map[string]interface{}{}
+
+	if len(tweet.Medias) > 0 {
+		for _, media := range tweet.Medias {
+			media_entities = append(media_entities, map[string]interface{}{
+				"media_id":     strconv.Itoa(media.ID),
+				"tagged_users": []string{},
+			})
+		}
+	}
+
+	post_medias := map[string]interface{}{
+		"media_entities":     media_entities,
+		"possibly_sensitive": false,
+	}
+
+	variables := map[string]interface{}{
+		"dark_request":            false,
+		"media":                   post_medias,
+		"semantic_annotation_ids": []string{},
+		"tweet_text":              tweet.Text,
+	}
+
+	features := map[string]interface{}{
+		"communities_web_enable_tweet_community_results_fetch":                    true,
+		"c9s_tweet_anatomy_moderator_badge_enabled":                               true,
+		"tweetypie_unmention_optimization_enabled":                                true,
+		"responsive_web_edit_tweet_api_enabled":                                   true,
+		"graphql_is_translatable_rweb_tweet_is_translatable_enabled":              true,
+		"view_counts_everywhere_api_enabled":                                      true,
+		"longform_notetweets_consumption_enabled":                                 true,
+		"responsive_web_twitter_article_tweet_consumption_enabled":                true,
+		"tweet_awards_web_tipping_enabled":                                        false,
+		"creator_subscriptions_quote_tweet_preview_enabled":                       false,
+		"longform_notetweets_rich_text_read_enabled":                              true,
+		"longform_notetweets_inline_media_enabled":                                true,
+		"articles_preview_enabled":                                                true,
+		"rweb_video_timestamps_enabled":                                           true,
+		"rweb_tipjar_consumption_enabled":                                         true,
+		"responsive_web_graphql_exclude_directive_enabled":                        true,
+		"verified_phone_label_enabled":                                            false,
+		"freedom_of_speech_not_reach_fetch_enabled":                               true,
+		"standardized_nudges_misinfo":                                             true,
+		"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled": true,
+		"responsive_web_graphql_skip_user_profile_image_extensions_enabled":       false,
+		"responsive_web_graphql_timeline_navigation_enabled":                      true,
+		"responsive_web_enhance_cards_enabled":                                    false,
+	}
+
+	body := map[string]interface{}{
+		"reply": map[string]interface{}{
+			"in_reply_to_tweet_id":   tweetID,
+			"exclude_reply_user_ids": []string{},
+		},
+		"features":  features,
+		"variables": variables,
+		"queryId":   "F7hteriqzdRzvMfXM6Ul4w",
+	}
+
+	b, _ := json.Marshal(body)
+	req.Body = io.NopCloser(bytes.NewReader(b))
+
+	var response newTweet
+	err = s.RequestAPI(req, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	if result := response.parse(); result != nil {
+		return result, nil
+	}
+
+	return nil, errors.New("tweet wasn't post")
+}
 
 func (s *Scraper) CreateTweet(tweet NewTweet) (*Tweet, error) {
 	req, err := s.newRequest("POST", "https://twitter.com/i/api/graphql/oB-5XsHNAbjvARJEc8CZFw/CreateTweet")
